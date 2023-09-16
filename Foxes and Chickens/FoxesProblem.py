06b2894c147d0c38cc3b32452fcdcbc3b0d1afa4
brendanshaw14 <brendanshaw18@gmail.com>
2023-09-15 21:15:52 -0400

    get successors appears to be working

--- Foxes and Chickens/FoxesProblem.py
+++ Foxes and Chickens/FoxesProblem.py
@@ -1,4 +1,7 @@
 class FoxesProblem:
+    # global variables
+    actions = [(-2, 0, -1), (-1, 0, -1), (-1, -1, -1), (0, -1, -1), (0, -2, -1)]
+
     def __init__(self, start_state=(3, 3, 1)):
         self.start_state = start_state
         self.goal_state = (0, 0, 0)
@@ -10,27 +13,24 @@ class FoxesProblem:
     def get_successors(self, state):
         successors = list()
         if state[2] == 1:
-            actions = [(-2, 0, -1), (-1, 0, -1), (-1, -1, -1), (0, -1, -1), (0, -2, -1)]
-            for action in actions:
-                foxes = state[0]+action[0]
-                chickens = state[1]+action[1]
-                if foxes >= 0 and chickens >= 0 and foxes <= chickens: 
-                    successors.append((foxes, chickens, 0))
+            for action in self.actions:
+                successor = ((state[0]+action[0], state[1]+action[1], 0))
+                if self.is_valid_state(successor) and successor != self.start_state:
+                    successors.append(successor)
         else:
-            actions = [(2, 0, 1), (1, 0, 1), (1, 1, 1), (0, 1, 1), (0, 2, 1)]
-            for action in actions:
-                foxes = self.start_state[0]-(state[0])+action[0]
-                chickens = state[1]+action[1]
-                if foxes >= 0 and chickens >= 0 and foxes <= chickens: 
-                    successors.append((foxes, chickens, 1))
+            for action in self.actions:
+                successor = (state[0]-action[0], state[1]-action[1], 1)
+                if self.is_valid_state(successor) and successor != self.start_state:
+                    successors.append(successor)
         return successors
 
     def is_valid_state(self, state):
         # is the left side good? 
-        if (state[0] <= state[1]) or state[1] == 0: 
-            #is the right side good?
-            if (self.start_state[0] - state[0]) <= (self.start_state[1]-state[1]) or (self.start_state[0] - state[0]) == 0:
-                return True
+        if state[0] >= 0 and state[1] >= 0 and state[2] >= 0:
+            if (state[0] <= state[1]) or state[1] == 0: 
+                #is the right side good?
+                if (self.start_state[0] - state[0]) <= (self.start_state[1]-state[1]) or (self.start_state[1] - state[1]) == 0:
+                    return True
         return False
 
 
@@ -42,6 +42,5 @@ class FoxesProblem:
 if __name__ == "__main__":
     test1= FoxesProblem((3, 3, 1))
     print(test1.get_successors((3, 3, 1)))
+    print(test1.get_successors((2, 2, 0)))
 
-    test2= FoxesProblem((2, 1, 0))
-    print(test2.get_successors((2, 1, 0)))
